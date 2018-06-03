3250f5c48ffa019574cdd0381fd0e27952bc5928
Regina Battle <hello@reginabattle.com>
2018-06-03 19:36:20 -0400

    Add gulp task to compile and watch SASS

--- gulpfile.js
+++ gulpfile.js
@@ -4,22 +4,35 @@ const gulp = require('gulp');
 const rename = require('gulp-rename');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify-es').default;
+const sass = require('gulp-sass');
 
 // Concatinate scripts
-gulp.task("concatScripts", () => {
+gulp.task('concatScripts', () => {
 	gulp.src('js/*.js')
 	.pipe(concat('scripts.js'))
 	.pipe(gulp.dest('js'))
 });
 
 // Minify scripts
-gulp.task("minifyScripts", () => {
+gulp.task('minifyScripts', () => {
 	gulp.src('js/scripts.js')
 	.pipe(rename('scripts.min.js'))
-	.pipe(uglify())
+	.pipe(uglify().on('error', uglify.logError))
 	.pipe(gulp.dest('js'))
 });
 
-gulp.task("default", ["hello"], () => {
+// Compile SASS
+gulp.task('compileSASS', () => {
+	gulp.src('sass/*.scss')
+	.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+	.pipe(gulp.dest('css'))
+});
+
+// Watch SASS
+gulp.task('watchSASS', function () {
+  gulp.watch('./sass/**/*.scss', ['compileSASS']);
+});
+
+gulp.task("default", ['concatScripts', 'minifyScripts'], () => {
 	console.log("This is the default task.")
 });
