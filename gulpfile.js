df516efc32ccdacf2c3ad9f885140079890402df
Regina Battle <hello@reginabattle.com>
2018-06-03 19:13:12 -0400

    Add gulp task to minify scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,22 @@
 'use strict';
 
 const gulp = require('gulp');
+const rename = require('gulp-rename');
 const concat = require('gulp-concat');
+const uglify = require('gulp-uglify-es').default;
 
+// Concatinate scripts
 gulp.task("concatScripts", () => {
 	gulp.src('js/*.js')
 	.pipe(concat('scripts.js'))
+	.pipe(gulp.dest('js'))
+});
+
+// Minify scripts
+gulp.task("minifyScripts", () => {
+	gulp.src('js/scripts.js')
+	.pipe(rename('scripts.min.js'))
+	.pipe(uglify())
 	.pipe(gulp.dest('js'))
 });
 
